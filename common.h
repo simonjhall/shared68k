a5c6268e1224a4374dad08001ab674ebf87c9149
Simon Hall <simonjhall@gmail.com>
2018-12-19 19:41:53 +0000

    fix for proj file and added I2C headers

--- common.h
+++ common.h
@@ -43,6 +43,7 @@
 #define RAM_BASE	((void *)(0b010 << 21))
 #define RAM_SIZE	(1 * 1024 * 1024)
 
+#define HAS_PCI
 #define PCI_IO		(0b100 << 21)
 #define PCI_MEM		(0b101 << 21)
 #define PCI_CONFIG	(0b110 << 21)
@@ -51,7 +52,6 @@
 
 #define LOAD_POINT	((void *)((0b010 << 21) | 0x100))
 
-#define HAS_PCI
 
 #endif
 
@@ -68,9 +68,10 @@
 #define UART_TXE_BIT	(3)
 #define UART_PWREN_BIT	(4)
 
-#define I2C_BASE	((void *)(1 << 15))
-#define I2C_SDA_RD	(1)
-#define I2C_SDA_SCLK_WR	(1)
+#define HAS_I2C
+#define I2C_BASE		((unsigned char *)((1 << 15) | 2))
+#define I2C_SCLK_BIT	(0)
+#define I2C_SDA_BIT		(1)
 
 #define RAM_BASE	((void *)(1 << 23))
 #define RAM_SIZE 	(4 * 1024 * 1024)
